67eb603a9ec3b4a7fe06ccf307a3e36efec09c8e
Oliwia <oliwia.czap13@gmail.com>
2026-02-05 10:19:06 +0100

    Dodano obsługę ciężarówek do programu
    Wprowadzono klasę Ciezarowka dziedziczącą po Pojazd, z polem ladownoscTon oraz nadpisaną metodą WyswietlInformacje. Zmieniono wcześniejszy "string ladownoscTon" na "int ladownoscTon". Dodano przykładowe obiekty samochodów osobowych oraz ciężarówek do listy pojazdów w Main. Kod został uporządkowany poprzez dodanie komentarzy rozdzielających typy pojazdów.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@
     {
         List<Pojazd> pojazdy = new List<Pojazd>();
 
+        // Samochody Osobowe
         pojazdy.Add(new SamochodOsobowy("Toyota", "Corolla", 2020, "4"));
-        
+        pojazdy.Add(new SamochodOsobowy("Honda", "Civic", 2019, "2"));
+        // Ciężarówki
+        pojazdy.Add(new Ciezarowka("Volvo", "FH16", 2018, 25));
+        pojazdy.Add(new Ciezarowka("Scania", "R500", 2021, 30));
+
+
         // foreach przechodzi po liście i wyświetla dane z metody WyswietlInfomracje() dla danego pojazdu
         foreach (Pojazd element in pojazdy)
         {
@@ -48,11 +54,19 @@ class SamochodOsobowy : Pojazd
         return base.WyswietlInformacje() + $", Liczba drzwi: {lizbaDrzwi}";
     }
 }
-/*
+
 class Ciezarowka : Pojazd
 {
-    private string ladownoscTon;
+    private int ladownoscTon;
     // wywołanie konstruktora klasy bazowej Pojazd
+    public Ciezarowka(string marka, string model, int rokProdukcji, int ladownoscTon) : base (marka, model, rokProdukcji)
+    {
+        this.ladownoscTon = ladownoscTon;
+    }
 
     // podanie danych do WyswietlInfomracje()
-}*/
+    public override string WyswietlInformacje()
+    {
+        return base.WyswietlInformacje() + $", Ładowność: {ladownoscTon} ton";
+    }
+}
