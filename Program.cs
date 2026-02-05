62356702e51c6f7831b8c381f58b942b0a747a0c
Oliwia <oliwia.czap13@gmail.com>
2026-02-05 09:56:09 +0100

    Dodano szkic klas pojazdów i usunięto kod startowy
    Usunięto domyślny kod konsoli i dodano klasy Program, Pojazd, SamochodOsobowy oraz Ciezarowka jako szkic do dalszej implementacji. Przygotowano strukturę dziedziczenia oraz pola dla pojazdów, pozostawiając komentarze do rozbudowy funkcjonalności.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+       static void Main(string[] args)
+    {
+        // lista obiektó typu pojazd
+        // przynajmniej 1 osobowy 1 ciezarowy
+        // foreach przechodzi po liście i wyświetla dane z metody WyswietlInfomracje() dla danego pojazdu
+    }
+}
+class Pojazd
+{
+    static void Main(string[] args)
+    {
+
+    }
+    private string marka;
+    private string model;
+    private int rokProdukcji;
+
+    public Pojazd(string marka, string model, int rokProdukcji)
+    {
+        this.marka = "";
+        this.model = "";
+        this.rokProdukcji = 0;
+    }
+}
+
+class SamochodOsobowy : Pojazd
+{
+    private string lizbaDrzwi;
+    // wywołanie konstruktora klasy bazowej Pojazd
+    // podanie danych do WyswietlInfomracje()
+}
+
+class Ciezarowka : Pojazd
+{
+    private string ladownoscTon;
+    // wywołanie konstruktora klasy bazowej Pojazd
+    // podanie danych do WyswietlInfomracje()
+}
