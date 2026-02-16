b15efee3b2b4cf89f8a98292b724af082c54ab29
Oliwia <oliwia.czap13@gmail.com>
2026-02-16 08:32:38 +0100

    Refactor vehicle list initialization and comments

--- Program.cs
+++ Program.cs
@@ -4,20 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Pojazdy> pojazdy = new List<Pojazdy>();
+        List<Pojazdy> pojazdy = new List<Pojazdy>(); // tworzy listę od clasy pojazdy o nazwie "pojazdy" = tworzy nową listę 
 
         // Samochody Osobowe
-        pojazdy.Add(new SamochodOsobowy("Toyota", "Corolla", 2020, "4"));
+        pojazdy.Add(new SamochodOsobowy("Toyota", "Corolla", 2020, "4")); // pojazdy.Add - dodaj do listy "pojazdy"; new SamochodOsobowy - nowy element + parametry + dodatkowy parametr
         pojazdy.Add(new SamochodOsobowy("Honda", "Civic", 2019, "2"));
         // Ciężarówki
-        pojazdy.Add(new Ciezarowka("Volvo", "FH16", 2018, 25));
+        pojazdy.Add(new Ciezarowka("Volvo", "FH16", 2018, 25)); // pojazdy.Add - dodaj do listy "pojazdy"; new Ciezarowka - nowy element + parametry +dodatkowy parametr
         pojazdy.Add(new Ciezarowka("Scania", "R500", 2021, 30));
 
 
         // foreach przechodzi po liście i wyświetla dane z metody WyswietlInfomracje() dla danego pojazdu
-        foreach (Pojazdy element in pojazdy)
+        foreach (Pojazdy element in pojazdy) // dla każdego (parametr kolejny_element_listy w Nazwa_listy)
         {
-            Console.WriteLine(element.WyswietlInformacje());
+            Console.WriteLine(element.WyswietlInformacje()); 
         }
     }
 }
