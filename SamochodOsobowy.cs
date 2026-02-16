9184fc5bed329eb57c8a2a541605e9af1eed343a
Oliwia <oliwia.czap13@gmail.com>
2026-02-16 08:41:19 +0100

    Refactor SamochodOsobowy class with comments

--- SamochodOsobowy.cs
+++ SamochodOsobowy.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace TransPol
 {
-    internal class SamochodOsobowy : Pojazdy
+    internal class SamochodOsobowy : Pojazdy // dziedziczy po pojazdach
     {
-        private string lizbaDrzwi;
+        private string lizbaDrzwi; // string tylko dla tej klasy
 
         // wywołanie konstruktora klasy bazowej Pojazd
-        public SamochodOsobowy(string marka, string model, int rokProdukcji, string liczbaDrzwi) : base(marka, model, rokProdukcji)
+        public SamochodOsobowy(string marka, string model, int rokProdukcji, string liczbaDrzwi) : base(marka, model, rokProdukcji) // base - co było w dziedziczonej klasie
         {
-            this.lizbaDrzwi = liczbaDrzwi;
+            this.lizbaDrzwi = liczbaDrzwi; // bierzemy stąd liczbę drzwi i przypisujemy do końcowych parametrów
         }
 
         // podanie danych do WyswietlInfomracje()
-        public override string WyswietlInformacje()
+        public override string WyswietlInformacje() // override - dopisz do WyswietlInformacje()
         {
-            return "Samochód osobowy: " + base.WyswietlInformacje() + $", Liczba drzwi: {lizbaDrzwi}";
+            return "Samochód osobowy: " + base.WyswietlInformacje() + $", Liczba drzwi: {lizbaDrzwi}"; // najpiew nazwa do czego jest ta konkretna klasa, wszystko z klasy pojazdy, przypisana nowa zmienna
         }
     }
 }
